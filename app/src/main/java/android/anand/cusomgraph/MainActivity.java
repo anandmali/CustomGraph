a38cd603ce38d32d84243686d85248e5b2d1a752
anandmali <anandm475@gmail.com>
2022-02-05 12:49:44 +0530

    Migration: Migrated to AndroidX, upgraded gradle plugin and  dependencies, code hygiene improvements.

--- app/src/main/java/android/anand/cusomgraph/MainActivity.java
+++ app/src/main/java/android/anand/cusomgraph/MainActivity.java
@@ -1,9 +1,8 @@
 package android.anand.cusomgraph;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,28 +23,7 @@ public class MainActivity extends AppCompatActivity {
      * Array to hold data fpr graph
      */
     public float[] GraphValues() {
-        return new float[] { 3, 5, 4, 8, 5, 7, 4 };
+        return new float[]{3, 5, 4, 8, 5, 7, 4};
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
